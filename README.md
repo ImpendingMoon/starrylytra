c1f5d3cac8a9d421dd1d2e7c2c0cc07796181202
ImpendingMoon <theimpendingmoon@gmail.com>
2025-07-28 21:11:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# starrylytra
+Minecraft mod which replaces the Elytra material with the End Portal material
---
+++ src/client/java/com/impendingmoon/starrylytra/StarrylytraClient.java
@@ -0,0 +1,10 @@
+package com.impendingmoon.starrylytra;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StarrylytraClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
